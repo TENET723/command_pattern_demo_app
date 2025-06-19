39ed33f69b05ec3455b9560775c61693f0697827
TENET723 <thetnaingtun.job@gmail.com>
2025-06-19 21:52:06 +0630

    add auto select and add shortcut

--- CommandPatternDemo/Commands/AddBoxCommand.cs
+++ CommandPatternDemo/Commands/AddBoxCommand.cs
@@ -5,14 +5,20 @@ public class AddBoxCommand : ICommand
 {
     private readonly List<Box> _boxes;
     private readonly Box _box;
+    private Action<Box>? _onExecute;
 
-    public AddBoxCommand(List<Box> boxes, Box box)
+    public AddBoxCommand(List<Box> boxes, Box box, Action<Box>? onExecute = null)
     {
         _boxes = boxes;
         _box = box;
+        _onExecute = onExecute;
     }
 
-    public void Execute() => _boxes.Add(_box);
+    public void Execute()
+    {
+        _boxes.Add(_box);
+        _onExecute?.Invoke(_box);
+    }
 
     public void Undo() => _boxes.Remove(_box);
 }
